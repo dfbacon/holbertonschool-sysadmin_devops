3984a14d8de3fcce52edc14a8a42adec0918e8d8
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-27 21:10:49 +0000

    remove single quotes

--- 0x0D-SQL_more_queries/0-privileges.sql
+++ 0x0D-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Show priveleges for defined users
 
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
