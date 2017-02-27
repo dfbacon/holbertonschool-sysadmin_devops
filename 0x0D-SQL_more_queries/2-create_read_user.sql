67c216756023e4acda9c01fdcc5a2a0882b1130d
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-27 21:11:42 +0000

    ex2

---
+++ 0x0D-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Create hbtn_0d_2 table and user_0d_2.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
