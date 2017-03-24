d0df71669f0f1a34fcc23b52ab324b3ca609f05a
Daniel Bacon <dbacon338@gmail.com>
2017-03-24 19:15:52 +0000

    corrected file path

--- 0x13-configuration_management/0-create_a_file.pp
+++ 0x13-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Creates a file, 'holberton', in the /tmp directory
-file { '/tmp/holberton/':
+file { '/tmp/holberton':
   ensure  => file,
-  path    => '~/tmp/holberton',
+  path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet\n'
 }
