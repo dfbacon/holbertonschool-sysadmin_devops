d3271bcac49c8ee7d1c0f9a935406f8f8d2ffbe8
Daniel Bacon <dbacon338@gmail.com>
2017-04-10 07:06:58 +0000

    access data as JSON

--- 0x17-api/0-gather_data_from_an_API.py
+++ 0x17-api/0-gather_data_from_an_API.py
@@ -21,11 +21,40 @@ Additional Requirements:
 '''
 if __name__ == "__main__":
     import requests
-    var root = 'https://jsonplaceholder.typicode.com';
+    import json
+    import sys
 
-    $.ajax({
-        url: root + '/posts/1',
-        method: 'GET'
-    }).then(function(data) {
-        console.log(data);
-    });
+    if len(sys.argv) != 2:
+        print("Usage: ./0-gather_data_from_an_API.py employee_id")
+        exit(1)
+
+    url_todo = 'https://jsonplaceholder.typicode.com/todos'
+    url_users = 'https://jsonplaceholder.typicode.com/users'
+    user_id = int(sys.argv[1])
+
+    try:
+        r_users = requests.get(url_users)
+        r_todo = requests.get(url_todo)
+        total_count = 0
+        completed_count = 0
+
+        for user in r_users.json():
+            if user['id'] is user_id:
+                user_name = user['name']
+
+        for task in r_todo.json():
+            if task['userId'] is user_id:
+                total_count += 1
+                if task['completed'] is True:
+                    completed_count += 1
+
+        print("Employee {} is done with tasks({}/{}):".format(
+            user_name, completed_count, total_count))
+
+        for task_name in r_todo.json():
+            if task_name['userId'] is user_id:
+                if task_name['completed'] is True:
+                    print("\t{}".format(task_name['title']))
+
+    except:
+        pass
