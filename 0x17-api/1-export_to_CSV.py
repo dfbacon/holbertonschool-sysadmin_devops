2b70c3776b6087865e9e90cef9ef805365a71a6e
Daniel Bacon <dbacon338@gmail.com>
2017-04-10 08:53:57 +0000

    simplified, converting to .csv

--- 0x17-api/1-export_to_CSV.py
+++ 0x17-api/1-export_to_CSV.py
@@ -15,7 +15,6 @@ Additional Requirements:
 
 if __name__ == "__main__":
     import requests
-    import json
     import csv
     import sys
 
@@ -23,25 +22,17 @@ if __name__ == "__main__":
         print("Usage: ./1-export_to_CSV.py employee_id")
         exit(1)
 
-    url_todo = 'https://jsonplaceholder.typicode.com/todos'
-    url_users = 'https://jsonplaceholder.typicode.com/users'
-    user_id = int(sys.argv[1])
-
     try:
-        r_users = requests.get(url_users)
-        r_todo = requests.get(url_todo)
-        file_name = str(user_id) + '.csv'
-
-        for user in r_users.json():
-            if user['id'] is user_id:
-                user_name = user['name']
-
-        with open(file_name, 'w+', newline="") as f:
+        url = "https://jsonplaceholder.typicode.com/users"
+        name = requests.get("{}/{}".format(
+            url, sys.argv[1])).json().get("username")
+        r_todo = requests.get("{}/{}/todos".format(
+                url, sys.argv[1])).json()
+        with open ("{}.csv".format(sys.argv[1]), mode="w", newline="") as f:
             writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-            for row in r_todo.json():
-                if row['userId'] is user_id:
-                    writer.writerows(row['userId'], user_name,
-                                     row['completed'], row['title'])
+            writer.writerows([sys.argv[1], name,
+                                 e.get("completed"),
+                                 e.get("title")] for e in r_todo)
 
     except:
         pass
